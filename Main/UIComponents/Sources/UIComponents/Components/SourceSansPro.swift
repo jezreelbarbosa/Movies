e603a4c098c83438b25d0f5d71dce42a3c710fa7
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2021-10-18 10:18:09 -0400

    Implemented SourceSansPro font

---
+++ Main/UIComponents/Sources/UIComponents/Components/SourceSansPro.swift
@@ -0,0 +1,25 @@
+//
+//  SourceSansPro.swift
+//  UIComponent
+//
+//  Created by Jezreel Barbosa on 18/10/21.
+//
+
+import Foundation
+import UIKit
+
+public enum SourceSansPro: String, FontRegistrable {
+
+    case extraLight
+    case extraLightItalic
+    case light
+    case lightItalic
+    case regular
+    case italic
+    case semiBold
+    case semiBoldItalic
+    case bold
+    case boldItalic
+    case black
+    case blackItalic
+}
